412b8a88d632c7859d826154cc110a30c436d521
vinhuang0228 <vinhuang0228@gmail.com>
2021-12-06 20:54:37 +0800

    update services and infra request

--- app/application/services/list_minipairs.rb
+++ app/application/services/list_minipairs.rb
@@ -4,35 +4,53 @@ require 'dry/monads'
 
 module CryptoExpert
   module Service
-    # Retrieves array of all listed project entities
+    # Retrieves array of all listed minipair signal entities
     class ListMiniPairs
       include Dry::Transaction
-
-      step :list_minipair
-      step :view_minipair
+      
+      # TODO: pair list service fix
+      
+      step :validate_list
+      step :list_minipair_signal
+      # step :view_minipair
 
       private
-      def list_minipair(input)
-        minipairs = input.map do |pair|
+
+      NO_PAIR_ERR = 'Add a Mini Pair to get started'
+      VIEW_PAIR_ERR = 'Can not make viewable pairs'
+
+      # Expects list of movies in input[:list_request]
+      def validate_list(input)
+        list_request = input[:list_request].call
+        if list_request.success?
+          Success(input.merge(list: list_request.value!))
+        else
+          Failure(list_request.failure)
+        end
+      end
+
+      def list_minipair_signal(input)
+        list = input[:list]
+        list.map do |pair|
           Binance::MiniPairMapper.new(pair).get
-          # Repository::For.klass(Entity::TempMiniPair).find_symbol(pair)
-        end.compact
-
-      Success(minipairs)
+        end
+          .then { |minipairs| Response::MinipairsList.new(minipairs) }
+          .then { |list| Response::ApiResult.new(status: :ok, message: list) }
+          .then { |result| Success(result) }
+        # Success(Response::ApiResult.new(status: :created, message: minipairs))
       rescue StandardError => e
         puts e.backtrace.join("\n")
-        Failure('Add a Mini Pair to get started')
+        Failure(NO_PAIR_ERR)
       end
 
-      def view_minipair(input)
-        viewable_minipairs = Views::MiniPairList.new(input)
-
-      Success(viewable_minipairs)
-      rescue StandardError => e
-        puts e.backtrace.join("\n")
-        Failure('Can not make viewable pairs')
-      end
-
+      # def view_minipair(input)
+      #   viewable_minipairs = Views::MiniPairList.new(input)
+      #   Success(viewable_minipairs)
+      # rescue StandardError => e
+      #   puts e.backtrace.join("\n")
+      #   Failure(VIEW_PAIR_ERR)
+      # end
+      
     end
   end
 end
---
+++ app/infrastructure/crypto_expert_api.rb
@@ -0,0 +1,88 @@
+# frozen_string_literal: true
+
+# TODO: build gateway from api layer
+
+# frozen_string_literal: true
+
+require_relative 'list_request'
+require 'http'
+
+module CryptoExpert
+  module Gateway
+    # Infrastructure to call CodePraise API
+    class Api
+      def initialize(config)
+        @config = config
+        @request = Request.new(@config)
+      end
+
+      def alive?
+        @request.get_root.success?
+      end
+
+      def minipair_list(list)
+        @request.minipair_list(list)
+      end
+
+      def get_minipair(symbol)
+        @request.get_minipair(symbol)
+      end
+
+      # HTTP request transmitter
+      class Request
+        def initialize(config)
+          @api_host = config.API_HOST
+          @api_root = config.API_HOST + '/api/v1'
+        end
+
+        def get_root # rubocop:disable Naming/AccessorMethodName
+          call_api('get')
+        end
+
+        def minipair_list(list)
+          call_api('get', ['minipair'],
+                   'list' => Value::WatchedList.to_encoded(list))
+        end
+        
+        def get_minipair(symbol)
+            call_api('post', ['minipair', symbol])
+        end
+
+        private
+
+        def params_str(params)
+          params.map { |key, value| "#{key}=#{value}" }.join('&')
+            .then { |str| str ? '?' + str : '' }
+        end
+
+        def call_api(method, resources = [], params = {})
+          api_path = resources.empty? ? @api_host : @api_root
+          url = [api_path, resources].flatten.join('/') + params_str(params)
+          HTTP.headers('Accept' => 'application/json').send(method, url)
+            .then { |http_response| Response.new(http_response) }
+        rescue StandardError
+          raise "Invalid URL request: #{url}"
+        end
+      end
+
+      # Decorates HTTP responses with success/error
+      class Response < SimpleDelegator
+        NotFound = Class.new(StandardError)
+
+        SUCCESS_CODES = (200..299).freeze
+
+        def success?
+          code.between?(SUCCESS_CODES.first, SUCCESS_CODES.last)
+        end
+
+        def message
+          payload['message']
+        end
+
+        def payload
+          body.to_s
+        end
+      end
+    end
+  end
+end
