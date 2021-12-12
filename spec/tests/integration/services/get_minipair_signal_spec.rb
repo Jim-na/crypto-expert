9464eef628f6f133f7291cb6d67f22301e33c7ec
LLUVV <jimbo21xc30@gmail.com>
2021-12-12 23:58:11 +0800

    updat4e spec

--- spec/tests/integration/services/get_minipair_signal_spec.rb
+++ spec/tests/integration/services/get_minipair_signal_spec.rb
@@ -1,3 +1,26 @@
 # frozen_string_literal: true
 
-# TODO: spec test
+require_relative '../../../helpers/spec_helper.rb'
+describe 'Integration test of get minipair service and API gateway' do
+    it 'must get the minipair signal' do
+      input = {symbol: MINI_SYMBOL}
+      symbol_request = CryptoExpert::Forms::NewSymbol.new.call(input)
+      
+      # WHEN we request to add a project
+      res = CryptoExpert::Service::GetMiniPairSignal.new.call(symbol_request[:symbol])
+  
+      # THEN we should see a single project in the list
+      _(res.success?).must_equal true
+      minipair = res.value!
+      _(minipair[:symbol]).must_equal MINI_SYMBOL
+      _(minipair[:volume_change_percent]).wont_be_nil
+      _(minipair[:signal]).wont_be_nil
+      _(minipair[:time]).wont_be_nil
+      _(minipair[:spot_volume]).wont_be_nil
+      _(minipair[:spot_closeprice]).wont_be_nil
+      _(minipair[:funding_rate]).wont_be_nil
+      _(minipair[:longshort_ratio]).wont_be_nil
+      _(minipair[:open_interest]).wont_be_nil
+      _(minipair[:spot_change_percent]).wont_be_nil
+    end
+  end
